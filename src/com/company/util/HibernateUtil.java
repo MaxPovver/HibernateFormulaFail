5954c8e1dde388ace8a483713a1bdd9521047952
Maxim Chistov <m.chistov@fundcount.com>
2017-03-27 21:25:40 +0300

    fixed gradle

---
+++ src/com/company/util/HibernateUtil.java
@@ -0,0 +1,26 @@
+/**
+ * <p>Title: FundCount, LLC</p>
+ * <p>Description: FundCount project</p>
+ * <p>Copyright: Copyright (c) 2001 FundCount, LLC</p>
+ * <p>Company: FundCount, LLC</p>
+ */
+package com.company.util;
+
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
