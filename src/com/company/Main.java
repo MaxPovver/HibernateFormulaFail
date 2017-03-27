98dcbf5fb669fe9175bb3dcc1e0c25832e96b182
Maxim Chistov <m.chistov@fundcount.com>
2017-03-27 21:53:06 +0300

    fixes to make it work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import static com.company.util.HibernateUtil.*;
+
+import com.company.data.*;
+import com.company.util.*;
+import org.hibernate.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Person person = new Person("Max", 22);
+        Transaction transaction = getSessionFactory().getCurrentSession().beginTransaction();
+        getSessionFactory().getCurrentSession().save(person);
+        transaction.commit();
     }
 }
